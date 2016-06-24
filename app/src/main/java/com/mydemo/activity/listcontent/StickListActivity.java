ea18b50b154feea47ad8cede0d627d28dba4c94d
kuronekoC <9059jl@163.com>
2016-06-24 16:09:47 +0800

    增加了类 测试

---
+++ app/src/main/java/com/mydemo/activity/listcontent/StickListActivity.java
@@ -0,0 +1,10 @@
+package com.mydemo.activity.listcontent;
+
+import android.app.Activity;
+import android.widget.ListView;
+
+/**
+ * Created by Administrator on 2016/6/24.
+ */
+public class StickListActivity extends Activity{
+}
